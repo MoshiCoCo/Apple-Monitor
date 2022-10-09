34a2f0152378a1d6fd4f6a5d27a0a097dc244ec5
Moshi <contact@misec.top>
2022-10-10 00:05:32 +0800

    解决while true 可能导致的cpu利用率升高

--- src/main/java/top/misec/applemonitor/AppleMonitorMain.java
+++ src/main/java/top/misec/applemonitor/AppleMonitorMain.java
@@ -2,15 +2,22 @@ package top.misec.applemonitor;
 
 import cn.hutool.cron.CronUtil;
 import cn.hutool.setting.Setting;
+import lombok.extern.slf4j.Slf4j;
 import top.misec.applemonitor.config.AppCfg;
 import top.misec.applemonitor.config.CfgSingleton;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author moshi
  */
 
-
+@Slf4j
 public class AppleMonitorMain {
+
+    private static final ReentrantLock LOCK = new ReentrantLock();
+    private static final Condition STOP = LOCK.newCondition();
 
     public static void main(String[] args) {
 
@@ -22,12 +29,18 @@ public class AppleMonitorMain {
 
             CronUtil.setCronSetting(setting);
             CronUtil.setMatchSecond(true);
-
             CronUtil.start(true);
-            while (true) {
 
+            LOCK.lock();
+            try {
+                STOP.await();
+            } catch (InterruptedException e) {
+                log.info("AppleMonitorMain is interrupted");
+            } finally {
+                LOCK.unlock();
             }
         }
+
 
     }
 
